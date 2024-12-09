680f99e8225d0ee17da09e0fa58f1d085aea96ad
Guilherme Freschi <guilhermefreschix@gmail.com>
2024-12-09 14:29:27 -0300

    Format currency to BRL standard

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 // Seleciona os elementos do formulário
 const amount = document.querySelector("#amount")
 
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
-  amount.value = value
+
+  // Transforma o valor em centavos
+  value = Number(value) / 100
+
+  amount.value = formatCurrencyBRL(value)
+}
+
+// Função para formatar o valor no padrão BRL
+function formatCurrencyBRL(value){
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  })
+
+  return value
 }
